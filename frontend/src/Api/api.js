86326e0645db615462a462876781fb5f90851e07
Ömer  ÖZTÜRK <omer.ozturk.or.ok@gmail.com>
2024-11-12 22:42:24 +0300

    rechange api address

--- frontend/src/Api/api.js
+++ frontend/src/Api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-// const API_URL = 'https://film-zk2t.onrender.com/api'
-const API_URL = 'http://localhost:5000/api'
+const API_URL = 'https://film-zk2t.onrender.com/api'
+// const API_URL = 'http://localhost:5000/api'
 
 export const fetchMovies = () => {
   return axios.get(`${API_URL}/movies`)
