2e6174e45a87bf910d2a56b87c37314f2c27fe67
Ömer  ÖZTÜRK <omer.ozturk.or.ok@gmail.com>
2024-11-15 22:45:11 +0300

    update web service api

--- frontend/src/Api/api.js
+++ frontend/src/Api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'https://film-zk2t.onrender.com/api'
+const API_URL = 'https://film-4dao.onrender.com/api'
 // const API_URL = 'http://localhost:5000/api'
 
 export const fetchMovies = () => {
